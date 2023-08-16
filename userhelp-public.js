97caa2932585f249bd72fa37f7e55c96c4daf1ca
jyant-labs <82305069+jyant-labs@users.noreply.github.com>
2023-08-16 18:56:03 +0700

    Add files via upload

--- userhelp-public.js
+++ userhelp-public.js
@@ -138,7 +138,7 @@ window.onload = function() {
     if(getMobileOperatingSystem() != "Android" && getMobileOperatingSystem() != "iOS") {
         yourCodeToBeCalled()
     } else {
-        loadJS('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js', yourCodeToBeCalled, document.head);
+        loadJS('https://html2canvas.hertzen.com/dist/html2canvas.min.js', yourCodeToBeCalled, document.head);
     }
 }
 
